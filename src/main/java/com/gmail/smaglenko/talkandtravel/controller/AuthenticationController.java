a59bd4db68fa3ee920910c1519d6503f7ec31a36
vitalii-smahlenko <smaglenko@gmail.com>
2023-10-26 15:58:31 +0300

    Add WebConfig & update SecurityConfiguration

--- src/main/java/com/gmail/smaglenko/talkandtravel/controller/AuthenticationController.java
+++ src/main/java/com/gmail/smaglenko/talkandtravel/controller/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/authentication")
 @RequiredArgsConstructor
-@CrossOrigin
+//@CrossOrigin
 public class AuthenticationController {
     private final AuthenticationService authService;
     private final UserDtoMapper mapper;
--- src/main/java/com/gmail/smaglenko/talkandtravel/controller/UserController.java
+++ src/main/java/com/gmail/smaglenko/talkandtravel/controller/UserController.java
@@ -5,7 +5,7 @@ import com.gmail.smaglenko.talkandtravel.service.UserService;
 import com.gmail.smaglenko.talkandtravel.util.mapper.UserDtoMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin
+//@CrossOrigin
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
